903e9f8c52ba3e84cb836b616bd307ffb7ae8e98
Sathiya3012 <sathiya@tecofize.com>
2025-06-10 11:19:54 +0530

    node app added

---
+++ app-2/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Hello, from app-2');
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
